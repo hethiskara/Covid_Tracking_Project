ef9224267eabea8bf638195d36e3ee73e50b990a
Hethiskarna Ravi <hethiskarnaravi@Hethiskarnas-MacBook-Air.local>
2024-09-23 19:00:07 -0700

    Deploy React app to GitHub Pages

---
+++ precache-manifest.09aec2985c11ea8c24491b3a3bdc9abe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83ba8e8cf96e2af824d09a5a56d85870",
+    "url": "/Covid_Tracking_Project/index.html"
+  },
+  {
+    "revision": "8136c9f2ef6231cf47bf",
+    "url": "/Covid_Tracking_Project/static/css/2.cee453c7.chunk.css"
+  },
+  {
+    "revision": "22fb12bc5259cc3ce067",
+    "url": "/Covid_Tracking_Project/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "8136c9f2ef6231cf47bf",
+    "url": "/Covid_Tracking_Project/static/js/2.22237953.chunk.js"
+  },
+  {
+    "revision": "8f3c00fcc8696eaf48fe80742cdb06a2",
+    "url": "/Covid_Tracking_Project/static/js/2.22237953.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22fb12bc5259cc3ce067",
+    "url": "/Covid_Tracking_Project/static/js/main.b4e59bcd.chunk.js"
+  },
+  {
+    "revision": "c85ae1c747a0a104f7c2",
+    "url": "/Covid_Tracking_Project/static/js/runtime-main.e5d2590b.js"
+  }
+]);
